bcb260412fc16e6e304631a034a09d838aca0e2b
vasu <bandaruvasu1@gmail.com>
2022-03-12 15:08:00 +0530

    make file

--- vpc/subnet.tf
+++ vpc/subnet.tf
@@ -1,8 +1,9 @@
 resource "aws_subnet" "subnets" {
-  depends_on = [aws_vpc_ipv4_cidr_block_association.addon]
-  count      = length(var.SUBNETS)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.SUBNETS, count.index)
+  depends_on             = [aws_vpc_ipv4_cidr_block_association.addon]
+  count                  = length(var.SUBNETS)
+  vpc_id                 = aws_vpc.main.id
+  cidr_block             = element(var.SUBNETS, count.index)
+  availability_zone      = element(var.AZS, count.index)
 
   tags = {
     Name = "subnet-${count.index}"
--- vpc/vars.tf
+++ vpc/vars.tf
@@ -2,3 +2,4 @@ variable "ENV" {}
 variable "VPC_CIDR_MAIN" {}
 variable "VPC_CIDR_ADDON" {}
 variable "SUBNETS" {}
+variable "AZS" {}
