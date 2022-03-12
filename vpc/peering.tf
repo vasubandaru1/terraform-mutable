64de5398dd435f0a2f9d8bf31ce0db84ab7cc274
vasu <bandaruvasu1@gmail.com>
2022-03-12 16:02:00 +0530

    make file

--- vpc/peering.tf
+++ vpc/peering.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc_peering_connection" "peering" {
   peer_vpc_id = aws_vpc.main.id
   vpc_id      = var.DEFAULT_VPC_ID
+  auto_accept = true
   tags = {
     Name = "${var.ENV}-vpc-to-default-vpc"
   }
