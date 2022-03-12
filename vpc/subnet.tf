6d2900368ff1eda31cef3cd753216fff6ada27cb
vasu <bandaruvasu1@gmail.com>
2022-03-12 11:56:06 +0530

    make file

--- vpc/subnet.tf
+++ vpc/subnet.tf
@@ -1,9 +1,12 @@
 resource "aws_subnet" "subnets" {
-  count            = length(var.SUBNETS)
-  vpc_id           = aws_vpc.main.id
-  cidr_block       =element(var.SUBNETS,count.index)
+  depends_on = [aws_vpc_ipv4_cidr_block_association.addon]
+  count      = length(var.SUBNETS)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.SUBNETS, count.index)
 
   tags = {
-      Name = "subnet-${count.index}"
+    Name = "subnet-${count.index}"
+  }
+
 }
-        }
+
