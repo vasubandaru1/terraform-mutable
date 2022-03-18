6bef6c01cff1fe4fe0dbe28cfcc6757b608a6a29
vasu <bandaruvasu1@gmail.com>
2022-03-18 09:03:09 +0530

    routes

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -1,23 +1,24 @@
 resource "aws_route_table" "route" {
   vpc_id = aws_vpc.main.id
 
-  route = []
+  route = [
+    {
+      "cidr_block"                       = var.DEFAULT_VPC_CIDR
+      "vpc_peering_connection_id"        = aws_vpc_peering_connection.peer.id
+      "carrier_gateway_id"               = ""
+      "destination_prefix_list_id"       = ""
+      "egress_only_gateway_id"           = ""
+      "gateway_id"                       = ""
+      "instance_id"                      = ""
+      "ipv6_cidr_block"                  = ""
+      "local_gateway_id"                 = ""
+      "nat_gateway_id"                   = ""
+      "network_interface_id"             = ""
+      "transit_gateway_id"               = ""
+      "vpc_endpoint_id"                  = ""
+    }
+  ]
   tags = {
     Name = "example"
   }
 }
-#{
-#"cidr_block"                       = var.DEFAULT_VPC_CIDR
-#"vpc_peering_connection_id"        = aws_vpc_peering_connection.peer.id
-#"carrier_gateway_id"               = ""
-#"destination_prefix_list_id"       = ""
-#"egress_only_gateway_id"           = ""
-#"gateway_id"                       = ""
-#"instance_id"                      = ""
-#"ipv6_cidr_block"                  = ""
-#"local_gateway_id"                 = ""
-#"nat_gateway_id"                   = ""
-#"network_interface_id"             = ""
-#"transit_gateway_id"               = ""
-#"vpc_endpoint_id"                  = ""
-#}
