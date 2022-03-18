427f5b903811e65b0a9d8d9f9b2c8e3d3819f3ab
vasu <bandaruvasu1@gmail.com>
2022-03-18 11:09:22 +0530

    routes

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -1,27 +1,19 @@
-resource "aws_route_table" "route" {
+resource "aws_route_table" "example" {
   vpc_id = aws_vpc.main.id
 
-  route = [
-    {
-      carrier_gateway_id                 = ""
-      "destination_prefix_list_id"        = ""
-      "egress_only_gateway_id"            = ""
-      "gateway_id"                        = ""
-      "instance_id"                       = ""
-      "ipv6_cidr_block"                   = ""
-      "local_gateway_id"                  = ""
-      "nat_gateway_id"                    = ""
-      "network_interface_id"              = ""
-      "transit_gateway_id"                = ""
-      "vpc_endpoint_id"                   = ""
-       cidr_block                        = var.DEFAULT_VPC_CIDR
-       vpc_peering_connection_id          = aws_vpc_peering_connection.peer.id
+  route {
+    cidr_block = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 
-    }
-  ]
+    gateway_id = ""
+  }
+
+  route {
+    ipv6_cidr_block        = ""
+    egress_only_gateway_id = ""
+  }
+
   tags = {
     Name = "example"
   }
 }
-
-
