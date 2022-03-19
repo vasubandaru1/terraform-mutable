590a2b62cc7d97bf71dd733e383fbb4c09519a95
vasu <bandaruvasu1@gmail.com>
2022-03-19 11:57:57 +0530

    routes

--- vpc/env/dev.tf
+++ vpc/env/dev.tf
@@ -1,6 +1,6 @@
 ENV               ="dev"
 VPC_CIDR_MAIN     ="10.0.0.0/16"
-VPC_CIDR_ADDON    =[]
+VPC_CIDR_ADDON    =["10.1.0.0/16","10.2.0.0/16"]
 SUBNETS           =["10.0.0.0/17", "10.0.128.0/17"]
 AZS               =["us-east-1c", "us-east-1d"]
 DEFAULT_VPC_ID    = "vpc-06fa7454f484e1fd4"
