5a97adf5d6a518619c95b29982d2b608865844d3
vasu <bandaruvasu1@gmail.com>
2022-03-19 12:00:45 +0530

    routes

--- vpc/locals.tf
+++ vpc/locals.tf
@@ -1,6 +1,6 @@
 locals {
   VPC_CIDR_MAIN = split(",", var.VPC_CIDR_MAIN)
-  ALL_VPC_CIDR  = split(",", var.VPC_CIDR_ADDON)
+  ALL_VPC_CIDR  = concat(local.VPC_CIDR_MAIN, var.VPC_CIDR_ADDON)
 }
 locals {
   association-list = flatten([
