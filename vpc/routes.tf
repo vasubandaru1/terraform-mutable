28a135ae23cfa6cd1d13e5c38c8678969e260f95
vasu <bandaruvasu1@gmail.com>
2022-03-20 13:37:04 +0530

    routes

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -15,44 +15,21 @@ resource "aws_route_table" "private-route" {
 resource "aws_route_table" "public-route" {
   vpc_id = aws_vpc.main.id
 
-  route = [
-  {
+  route {
     cidr_block                = var.DEFAULT_VPC_CIDR
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
     gateway_id                = ""
-    "carrier_gateway_id"      =""
-  "destination_prefix_list_id" =""
-  "egress_only_gateway_id"    =""
-   "instance_id"             = ""
-  "ipv6_cidr_block"            = ""
-    "local_gateway_id"         = ""
-    "nat_gateway_id"            = ""
-    "network_interface_id"       = ""
-    "transit_gateway_id"          =""
-    "vpc_endpoint_id"             = ""
-  },
-    {
-      cidr_block                = "0.0.0.0/0"
-      vpc_peering_connection_id = ""
-      gateway_id                 = aws_internet_gateway.igw.id
-      "carrier_gateway_id"      =""
-      "destination_prefix_list_id" =""
-      "egress_only_gateway_id"    =""
-      "instance_id"             = ""
-      "ipv6_cidr_block"            = ""
-      "local_gateway_id"         = ""
-      "nat_gateway_id"            = ""
-       "network_interface_id"       = ""
-      "transit_gateway_id"          =""
-      "vpc_endpoint_id"             = ""
-    }
-    ]
+  }
 
   tags = {
     Name = "public-route"
   }
 }
-
+#{
+#cidr_block = "0.0.0.0/0"
+#vpc_peering_connection_id = ""
+#gateway_id = aws_internet_gateway.igw.id
+#}
 
 
 
