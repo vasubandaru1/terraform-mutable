7973b020580877e71bfd6ddd65ed363c4710ae76
vasu <bandaruvasu1@gmail.com>
2022-03-21 11:27:42 +0530

    routes

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -1,14 +1,14 @@
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  name                 = "mydb"
-  username             = "admin"
-  password             = "admin123"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-}
+#resource "aws_db_instance" "default" {
+#  allocated_storage    = 10
+#  engine               = "mysql"
+#  engine_version       = "5.7"
+#  instance_class       = "db.t3.micro"
+#  name                 = "mydb"
+#  username             = "admin"
+#  password             = "admin123"
+#  parameter_group_name = "default.mysql5.7"
+#  skip_final_snapshot  = true
+#}
 
 
 resource "aws_db_parameter_group" "pg" {
