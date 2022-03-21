ebd963adfd48f9f15960ee9b58d5f6554a3e139c
vasu <bandaruvasu1@gmail.com>
2022-03-21 19:07:25 +0530

    routes

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -1,5 +1,6 @@
 resource "aws_db_instance" "mysql" {
   allocated_storage    = 10
+  identifier           = "mysql-${var.ENV}"
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
