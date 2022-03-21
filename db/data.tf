891eeea3c96791d7d969f7dadeb1e1401f09a499
vasu <bandaruvasu1@gmail.com>
2022-03-21 13:02:44 +0530

    routes

--- db/data.tf
+++ db/data.tf
@@ -2,8 +2,9 @@ data "terraform_remote_state" "VPC" {
   backend = "s3"
   config = {
     bucket = "vasudevops"
-    key    = "terraform-mutable/dev/db/terraform.tfstate"
+    key    = "terraform-mutable/db/dev/terraform.tfstate"
     region = "us-east-1"
 }
 
 }
+#terraform-mutable/db/dev/terraform.tfstate
