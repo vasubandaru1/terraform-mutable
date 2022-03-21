96f92864906d2d7ca4e4d6ac5f5aad88ff10b60c
vasu <bandaruvasu1@gmail.com>
2022-03-21 17:50:39 +0530

    routes

--- db/data.tf
+++ db/data.tf
@@ -15,7 +15,7 @@ data "aws_secretsmanager_secret_version" "secret-version" {
   secret_id = data.aws_secretsmanager_secret.secrets.id
 }
 resource "local_file" "secrets" {
-  content  = jsondecode(data.aws_secretsmanager_secret_version.secret-version)["RDS_MYSQL_USER"]
+  content  = jsondecode(data.aws_secretsmanager_secret_version.secret-version.secret_string)["RDS_MYSQL_USER"]
   filename = "tmp/1"
 }
 #output "secrets" {
