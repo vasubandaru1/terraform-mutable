0443b578324e7c14add0d238dfb769e1b5affa50
vasu <bandaruvasu1@gmail.com>
2022-03-21 11:33:42 +0530

    routes

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -3,25 +3,29 @@
 #  engine               = "mysql"
 #  engine_version       = "5.7"
 #  instance_class       = "db.t3.micro"
-#  name                 = "mydb"
+#  name                 = "mysqldb"
 #  username             = "admin"
 #  password             = "admin123"
 #  parameter_group_name = "default.mysql5.7"
 #  skip_final_snapshot  = true
 #}
 
+#
+#resource "aws_db_parameter_group" "pg" {
+#  name   = "mysql-${var.ENV}-pg"
+#  family = "mysql5.6"
+#
+#}
+#
+#resource "aws_db_subnet_group" "subnet-group" {
+#  name       = "mysql-subnet-group-${var.ENV}"
+#  subnet_ids = data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS
+#
+#  tags = {
+#    Name = "mysql-subnet-group-${var.ENV}"
+#  }
+#}
 
-resource "aws_db_parameter_group" "pg" {
-  name   = "mysql-${var.ENV}-pg"
-  family = "mysql5.6"
-
-}
-
-resource "aws_db_subnet_group" "subnet-group" {
-  name       = "mysql-subnet-group-${var.ENV}"
-  subnet_ids = data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS
-
-  tags = {
-    Name = "mysql-subnet-group-${var.ENV}"
-  }
+output "private_subnet_ids" {
+  value = data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS
 }
