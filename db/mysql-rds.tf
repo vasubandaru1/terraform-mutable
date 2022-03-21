9973e8af0b08a3cd4be3846d06f0ac81a1810fd8
vasu <bandaruvasu1@gmail.com>
2022-03-21 18:54:35 +0530

    routes

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -1,9 +1,9 @@
-resource "aws_db_instance" "default" {
+resource "aws_db_instance" "mysql" {
   allocated_storage    = 10
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  name                 = "mysql"
+  name                 = "dummy"
   username             = jsondecode(data.aws_secretsmanager_secret_version.secret-version.secret_string)["RDS_MYSQL_USER"]
   password             = jsondecode(data.aws_secretsmanager_secret_version.secret-version.secret_string)["RDS_MYSQL_PASS"]
   parameter_group_name = aws_db_parameter_group.pg.name
@@ -13,7 +13,7 @@ resource "aws_db_instance" "default" {
 
 resource "aws_db_parameter_group" "pg" {
   name   = "mysql-${var.ENV}-pg"
-  family = "mysql5.6"
+  family = "mysql5.7"
 
 }
 
