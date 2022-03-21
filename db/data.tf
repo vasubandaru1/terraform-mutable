883356322220ee40fe42f3d087ccb1d83411cfba
vasu <bandaruvasu1@gmail.com>
2022-03-21 16:57:42 +0530

    routes

--- db/data.tf
+++ db/data.tf
@@ -12,11 +12,11 @@ data "aws_secretsmanager_secret" "secrets" {
   name = var.ENV
 }
 resource "aws_secretsmanager_secret_version" "secrets-version" {
-  secret_id     = aws_secretsmanager_secret_version.secrets-version.id["RDS_MYSQL_USER"]
+  secret_id     = aws_secretsmanager_secret_version.secrets-version.id
 
 }
 output "example" {
-  value = jsondecode(aws_secretsmanager_secret_version.secrets-version.secret_string)
+  value = jsondecode(aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_MYSQL_USER"]
 }
 output "seccret" {
   value = data.aws_secretsmanager_secret.secrets
