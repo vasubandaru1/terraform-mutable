457fd71d8e2724898fe38ad92cd1f107ba724165
vasu <bandaruvasu1@gmail.com>
2022-03-23 13:21:11 +0530

    routes

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -16,11 +16,18 @@ resource "aws_route_table" "private-route" {
 resource "aws_route_table" "public-route" {
   vpc_id = aws_vpc.main.id
 
-  route {
+  route = [
+    {
     cidr_block                = var.DEFAULT_VPC_CIDR
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 
-  }
+  },
+    {
+      cidr_block                = [0.0.0.0/0]
+      vpc_peering_connection_id = ""
+
+    }
+    ]
 
   tags = {
     Name = "public-route"
