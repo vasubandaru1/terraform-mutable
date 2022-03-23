b8191d7e034ba5e581bf33018433967512b7824e
vasu <bandaruvasu1@gmail.com>
2022-03-23 13:21:44 +0530

    routes

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -23,7 +23,7 @@ resource "aws_route_table" "public-route" {
 
   },
     {
-      cidr_block                = [0.0.0.0/0]
+      cidr_block                = ["0.0.0.0/0"]
       vpc_peering_connection_id = ""
 
     }
