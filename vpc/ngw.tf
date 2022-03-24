a743e8320dbd8878697e7586c1da0adbf2132c5c
vasu <bandaruvasu1@gmail.com>
2022-03-24 10:02:46 +0530

    routes

---
+++ vpc/ngw.tf
@@ -0,0 +1,16 @@
+resource "aws_eip" "ngw" {
+  vpc      = true
+}
+
+ tags = {
+   Name = "${var.ENV}-ngw-ip"
+ }
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = aws_subnet.public_subnets.*.id[0]
+
+  tags = {
+    Name = "${var.ENV}-ngw"
+  }
+}
