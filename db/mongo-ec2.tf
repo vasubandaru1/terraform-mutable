aeccaa2a22d03efa8d75b20ac521a19280f96d4a
vasu <bandaruvasu1@gmail.com>
2022-03-25 12:38:26 +0530

    routes

--- db/mongo-ec2.tf
+++ db/mongo-ec2.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "mongodb" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    ipv6_cidr_blocks = []
     self             = false
     security_groups = []
     prefix_list_ids = []
