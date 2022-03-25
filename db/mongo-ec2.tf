1d72651c7a8c103791a68c1a8d2a2e8b79c4fb5f
vasu <bandaruvasu1@gmail.com>
2022-03-25 13:45:14 +0530

    routes

--- db/mongo-ec2.tf
+++ db/mongo-ec2.tf
@@ -3,7 +3,8 @@ resource "aws_security_group" "mongodb" {
   description = "mongodb-${var.ENV}"
   vpc_id      = data.terraform_remote_state.VPC.outputs.VPC_ID
 
-  ingress {
+  ingress = [
+    {
     description      = "MONGODB"
     from_port        = 27017
     to_port          = 27017
@@ -14,7 +15,19 @@ resource "aws_security_group" "mongodb" {
     security_groups =  []
     prefix_list_ids =  []
 
-  }
+  },
+    {
+      description      = SSH
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = local.ALL_CIDR
+      ipv6_cidr_blocks = []
+      self             = false
+      security_groups  = []
+      prefix_list_ids  = []
+    }
+    ]
 
   egress {
     description      = "egress"
