a77090a675d793ad2e14f2a13d223a5ca28f067d
vasu <bandaruvasu1@gmail.com>
2022-04-02 15:14:16 +0530

    routes

--- db/redis-elasticcache.tf
+++ db/redis-elasticcache.tf
@@ -10,7 +10,7 @@ resource "aws_elasticache_cluster" "redis" {
   security_group_ids = aws_security_group.redis.id
 }
 resource "aws_elasticache_parameter_group" "redis" {
-   name = "cache-params"
+   name = "redis-${var.ENV}"
    family = "redis6.x"
 
 }
