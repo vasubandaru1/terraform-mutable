c568f7856ae617826434b910761fd8c3b81f56a8
vasu <bandaruvasu1@gmail.com>
2022-04-02 09:18:03 +0530

    routes

--- db/redis-elasticcache.tf
+++ db/redis-elasticcache.tf
@@ -3,8 +3,8 @@ resource "aws_elasticache_cluster" "example" {
   engine               = "redis"
   node_type            = var.REDIS_INSTANCE_TYPE
   num_cache_nodes      = 1
-  parameter_group_name =   aws_elasticache_parameter_group.redis.name
-  subnet_group_name    =   aws_elasticache_subnet_group.redis.name
+  parameter_group_name = aws_elasticache_parameter_group.redis.name
+  subnet_group_name    = aws_elasticache_subnet_group.redis.name
   engine_version       = "6.x"
   port                 = 6379
   security_group_ids = [aws_security_group.redis.id]
@@ -12,7 +12,7 @@ resource "aws_elasticache_cluster" "example" {
 
 resource "aws_elasticache_parameter_group" "redis" {
   name   = "redis-${var.ENV}"
-  family = "redis6.x"
+  family = "redis 6.x"
 
 }
 
