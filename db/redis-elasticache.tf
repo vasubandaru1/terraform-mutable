4a77ee5eead69840f8194cc4ab4065b21d93f5c6
vasu <bandaruvasu1@gmail.com>
2022-04-03 14:41:33 +0530

    redis2

--- db/redis-elasticache.tf
+++ db/redis-elasticache.tf
@@ -70,49 +70,50 @@ resource "aws_elasticache_subnet_group" "redis" {
 #  subnet_ids = data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS
 #}
 #
-#resource "aws_security_group" "redis" {
-#  name        = "redis-${var.ENV}"
-#  description = "redis-${var.ENV}"
-#  vpc_id      = data.terraform_remote_state.VPC.outputs.VPC_ID
-#
-#  ingress = [
-#    {
-#      description      = "REDIS"
-#      from_port        = 6379
-#      to_port          = 6379
-#      protocol         = "tcp"
-#      cidr_blocks      = local.ALL_CIDR
-#      ipv6_cidr_blocks = []
-#      self             = false
-#      security_groups =  []
-#      prefix_list_ids =  []
-#
-#    }
-#
-#  ]
-#
-#  egress {
-#    description      = "egress"
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = []
-#    self             = false
-#    security_groups =  []
-#    prefix_list_ids =  []
-#  }
-#
-#
-#  tags = {
-#    Name = "redis-${var.ENV}"
-#  }
-#}
-#
-#resource "aws_route53_record" "redis" {
-#  zone_id = data.terraform_remote_state.VPC.outputs.INTERNAL_HOSTEDZONE_ID
-#  name    = "redis-${var.ENV}"
-#  type    = "CNAME"
-#  ttl     = "300"
-#  records = aws_elasticache_cluster.redis.cache_nodes.*.address
-#}
+
+resource "aws_security_group" "redis" {
+  name        = "redis-${var.ENV}"
+  description = "redis-${var.ENV}"
+  vpc_id      = data.terraform_remote_state.VPC.outputs.VPC_ID
+
+  ingress = [
+    {
+      description      = "REDIS"
+      from_port        = 6379
+      to_port          = 6379
+      protocol         = "tcp"
+      cidr_blocks      = local.ALL_CIDR
+      ipv6_cidr_blocks = []
+      self             = false
+      security_groups =  []
+      prefix_list_ids =  []
+
+    }
+
+  ]
+
+  egress {
+    description      = "egress"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    self             = false
+    security_groups =  []
+    prefix_list_ids =  []
+  }
+
+
+  tags = {
+    Name = "redis-${var.ENV}"
+  }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = data.terraform_remote_state.VPC.outputs.INTERNAL_HOSTEDZONE_ID
+  name    = "redis-${var.ENV}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = aws_elasticache_cluster.example.cache_nodes.*.address
+}
