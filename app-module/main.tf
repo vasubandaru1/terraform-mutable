497586ac7a7bdbd320324b447e7858ba2607dc1b
vasu <bandaruvasu1@gmail.com>
2022-04-04 11:01:58 +0530

    redis2

--- app-module/main.tf
+++ app-module/main.tf
@@ -1,10 +1,10 @@
-#resource "aws_spot_instance_request" "spot" {
-#  count                = var.SPOT_INSTANCE_COUNT
-#  ami                  = data.aws_ami.ami.id
-#  instance_type        = var.INSTANCE_TYPE
-#  subnet_id             = element(data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS,count.index)
-#  wait_for_fulfillment = true
-#}
+resource "aws_spot_instance_request" "spot" {
+  count                = var.SPOT_INSTANCE_COUNT
+  ami                  = data.aws_ami.ami.id
+  instance_type        = var.INSTANCE_TYPE
+  subnet_id             = element(data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS,count.index)
+  wait_for_fulfillment = true
+}
 
 resource "aws_instance" "od" {
   count                = var.OD_INSTANCE_COUNT
@@ -12,4 +12,12 @@ resource "aws_instance" "od" {
   instance_type        = var.INSTANCE_TYPE
   subnet_id            = element(data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS,count.index)
 
+}
+
+locals {
+  INSTANCE_IDS = concat(aws_spot_instance_request.spot.*.spot_instance_id, aws_instance.od.*.id)
+}
+
+output "INSTANCE_IDS" {
+  value = local.INSTANCE_IDS
 }
