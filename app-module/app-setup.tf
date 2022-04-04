0bf9b0d3ed6439c52585823bf255af0c13c377f1
vasu <bandaruvasu1@gmail.com>
2022-04-04 17:16:00 +0530

    redis2

---
+++ app-module/app-setup.tf
@@ -0,0 +1,13 @@
+resource "null_resource" "app-deploy" {
+  count = length(local.PRIVATE_IPS)
+  provisioner "remote-exec" {
+    connection {
+      host     = element(local.PRIVATE_IPS, count.index )
+      user     = local.ssh_user
+      password = local.ssh_pass
+    }
+    inline = [
+      "ansible-pull -u https://github.com/vasubandaru1/ANSIBLE2.git roboshop-pull.yml -e ENV=${var.ENV} -e COMPONENT=${var.COMPONENT}"
+    ]
+  }
+}
--- app-module/data.tf
+++ app-module/data.tf
@@ -12,6 +12,14 @@ data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "base"
   owners      = ["self"]
+
+}
+
+data "aws_secretsmanager_secret" "secrets" {
+  name = var.ENV
+}
+data "aws_secretsmanager_secret_version" "secret-version" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
 }
 
 #data "aws_ami" "ami" {
