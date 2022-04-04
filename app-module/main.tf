60f6e1af5be5526b426e25ebd0e6cdcb84c9aabb
vasu <bandaruvasu1@gmail.com>
2022-04-04 10:13:53 +0530

    redis2

--- app-module/main.tf
+++ app-module/main.tf
@@ -2,6 +2,6 @@ resource "aws_spot_instance_request" "spot" {
   count                = var.SPOT_INSTANCE_COUNT
   ami                  = data.aws_ami.ami.id
   instance_type        = var.INSTANCE_TYPE
-  subnet_id             = element(data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNET_IDS, count.index)
+  subnet_id             = element(data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS, count.index)
   wait_for_fulfillment = true
 }
