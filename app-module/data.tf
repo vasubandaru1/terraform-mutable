16ebb9381edf4b15c3cbb637314d743faae9d11f
vasu <bandaruvasu1@gmail.com>
2022-04-04 19:18:35 +0530

    redis2

--- app-module/data.tf
+++ app-module/data.tf
@@ -1,10 +1,20 @@
 data "terraform_remote_state" "VPC" {
   backend = "s3"
-  config = {
+  config  = {
     bucket = "vasudevops"
     key    = "terraform-mutable/vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
+  }
+
 }
+
+data "terraform_remote_state" "alb" {
+  backend = "s3"
+  config  = {
+    bucket = "vasudevops"
+    key    = "terraform-mutable/alb/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
 
 }
 
--- app-module/lb.tf
+++ app-module/lb.tf
@@ -11,3 +11,19 @@ resource "aws_lb_target_group_attachment" "tg-attach" {
   target_id        = element(local.INSTANCE_IDS,count.index )
   port             = var.port
 }
+
+resource "aws_lb_listener_rule" "private" {
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENV}.roboshop.internal"]
+    }
+  }
+}
