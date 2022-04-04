43ebf9f5471ab62b7491b3327fab1a95feb83685
vasu <bandaruvasu1@gmail.com>
2022-04-04 21:11:15 +0530

    redis2

--- app-module/lb.tf
+++ app-module/lb.tf
@@ -21,7 +21,7 @@ resource "aws_lb_target_group_attachment" "tg-attach" {
 }
 
 resource "aws_lb_listener_rule" "private" {
-  listener_arn = join(",", data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN)
+  listener_arn =  data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
   priority     = 100
 
   action {
