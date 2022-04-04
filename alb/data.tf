c6353d73e8c0b18abb35afdb6cb0804f77312702
vasu <bandaruvasu1@gmail.com>
2022-04-04 20:12:57 +0530

    redis2

--- alb/data.tf
+++ alb/data.tf
@@ -8,6 +8,17 @@ data "terraform_remote_state" "VPC" {
 
 }
 
+data "terraform_remote_state" "VPC" {
+  backend = "s3"
+  config = {
+    bucket = "vasudevops"
+    key    = "terraform-mutable/alb/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+}
+
+
 data "aws_secretsmanager_secret" "secrets" {
   name = var.ENV
 }
