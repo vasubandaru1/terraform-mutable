38b3d9a59057e35634eacfd537604d472d5319c7
vasu <bandaruvasu1@gmail.com>
2022-04-04 18:09:26 +0530

    redis2

---
+++ app-module/lb.tf
@@ -0,0 +1,12 @@
+resource "aws_lb_target_group" "tg" {
+  name     = local.tags["Name"]
+  port     = var.port
+  protocol = "HTTP"
+  vpc_id   = data.terraform_remote_state.VPC.outputs.VPC_ID
+}
+
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = element(local.INSTANCE_IDS,count.index )
+  port             = 80
+}
