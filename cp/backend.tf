cc8baaece7bb36d0887659b9dc87e5695674ed43
vasu <bandaruvasu1@gmail.com>
2022-04-06 21:44:31 +0530

    redis2

--- cp/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {}
-}
-
-provider "aws" {
-  region = "us-east-1"
-
-}
--- cp/env/dev.tf
+++
@@ -1,5 +0,0 @@
-ENV                   ="dev"
-#MONGODB_INSTANCE_TYPE  = "t2.micro"
-#RABBITMQ_INSTANCE_TYPE  = "t2.micro"
-#REDIS_INSTANCE_TYPE     ="t2.micro"
-
