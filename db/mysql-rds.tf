dba66a9c790919491aeb3c590c96b7a1f47de04a
vasu <bandaruvasu1@gmail.com>
2022-04-06 21:08:39 +0530

    redis2

---
+++ db/mysql-rds.tf
@@ -0,0 +1,91 @@
+locals {
+  rds_user = jsondecode(data.aws_secretsmanager_secret_version.secret-version.secret_string)["RDS_MYSQL_USER"]
+  rds_pass = jsondecode(data.aws_secretsmanager_secret_version.secret-version.secret_string)["RDS_MYSQL_PASS"]
+}
+
+
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 10
+  identifier           = "mysql-${var.ENV}"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "dummy"
+  #  username             = local.rds_user
+  #  password             = local.rds_pass
+  parameter_group_name = aws_db_parameter_group.pg.name
+  skip_final_snapshot  = true
+  #  vpc_security_group_ids = [aws_security_group.mysql.id]
+  #  db_subnet_group_name = aws_db_subnet_group.subnet-group.name
+}
+ resource "aws_db_parameter_group" "pg" {
+  name   = "mysql-${var.ENV}-pg"
+  family = "mysql5.7"
+
+}
+
+resource "aws_db_subnet_group" "subnet-group" {
+  name       = "mysql-subnet-group-${var.ENV}"
+  subnet_ids = data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS
+
+  tags = {
+    Name = "mysql-subnet-group-${var.ENV}"
+  }
+}
+
+
+
+resource "aws_security_group" "mysql" {
+  name        = "mysql-${var.ENV}"
+  description = "mysql-${var.ENV}"
+  vpc_id = data.terraform_remote_state.VPC.outputs.VPC_ID
+
+  ingress {
+    description      = "mysql"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = local.ALL_CIDR
+    ipv6_cidr_blocks = []
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "mysql-${var.ENV}"
+  }
+}
+
+
+
+
+
+
+
+
+
+resource "aws_route53_record" "mysql" {
+  zone_id = data.terraform_remote_state.VPC.outputs.INTERNAL_HOSTEDZONE_ID
+  name    = "mysql-${var.ENV}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_db_instance.mysql.address]
+}
+
+resource "null_resource" "schema-apply" {
+  provisioner "local-exec" {
+    command = <<EOF
+sudo yum install mariadb -y
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+cd /tmp
+unzip -o /tmp/mysql.zip
+mysql -h${aws_db_instance.mysql.address} -u${local.rds_user} -p${local.rds_pass} <mysql-main/shipping.sql
+EOF
+  }
+}
--- db/rabbitmq-ec2.tf
+++ db/rabbitmq-ec2.tf
@@ -1,95 +1,95 @@
-#resource "aws_security_group" "rabbitmq" {
-#  name        = "rabbitmq-${var.ENV}"
-#  description = "rabbitmq-${var.ENV}"
-#  vpc_id      = data.terraform_remote_state.VPC.outputs.VPC_ID
-#
-#  ingress = [
-#    {
-#      description      = "rabbitmq"
-#      from_port        = 5672
-#      to_port          = 5672
-#      protocol         = "tcp"
-#      cidr_blocks      = local.ALL_CIDR
-#      ipv6_cidr_blocks = []
-#      self             = false
-#      security_groups =  []
-#      prefix_list_ids =  []
-#
-#    },
-#    {
-#      description      = "SSH"
-#      from_port        = 22
-#      to_port          = 22
-#      protocol         = "tcp"
-#      cidr_blocks      = local.ALL_CIDR
-#      ipv6_cidr_blocks = []
-#      self             = false
-#      security_groups  = []
-#      prefix_list_ids  = []
-#    }
-#  ]
-#
-#  egress {
-#    description      = "egress"
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = []
-#    self             = false
-#    security_groups =  []
-#    prefix_list_ids =  []
-#  }
-#
-#
-#  tags = {
-#    Name = "rabbitmq-${var.ENV}"
-#  }
-#}
-#
-#resource "aws_spot_instance_request" "rabbitmq" {
-#  ami                    =data.aws_ami.ami.id
-#  instance_type          =var.RABBITMQ_INSTANCE_TYPE
-#  vpc_security_group_ids = [aws_security_group.rabbitmq.id]
-#  wait_for_fulfillment   = true
-#  subnet_id              = data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS[0]
-#
-#  tags = {
-#    Name = "rabbitmq-${var.ENV}"
-#  }
-#}
-#resource "aws_ec2_tag" "rabbitmq" {
-#  key         = "Name"
-#  resource_id = aws_spot_instance_request.rabbitmq.spot_instance_id
-#  value       = "rabbitmq-${var.ENV}"
-#}
-#
-#resource "aws_route53_record" "rabbitmq" {
-#  zone_id = data.terraform_remote_state.VPC.outputs.INTERNAL_HOSTEDZONE_ID
-#  name    = "rabbitmq-${var.ENV}"
-#  type    = "A"
-#  ttl     = "300"
-#  records = [aws_spot_instance_request.rabbitmq.private_ip]
-#}
-#
-#resource "null_resource" "rabbitmq_setup" {
-#  provisioner "remote-exec" {
-#    connection {
-#      host     = aws_spot_instance_request.rabbitmq.private_ip
-#      user     = local.ssh_user
-#      password = local.ssh_pass
-#    }
-#
-#
-#    inline = [
-#      "sudo yum install python3-pip -y",
-#      "sudo pip3 install pip --upgrade",
-#      "sudo pip3 install ansible",
-#      "ansible-pull -U https://github.com/vasubandaru1/ANSIBLE2.git roboshop-pull.yml -e ENV=${var.ENV} -e COMPONENT=rabbitmq"
-#
-#    ]
-#
-#  }
-#}
-#
-#
+resource "aws_security_group" "rabbitmq" {
+  name        = "rabbitmq-${var.ENV}"
+  description = "rabbitmq-${var.ENV}"
+  vpc_id      = data.terraform_remote_state.VPC.outputs.VPC_ID
+
+  ingress = [
+    {
+      description      = "rabbitmq"
+      from_port        = 5672
+      to_port          = 5672
+      protocol         = "tcp"
+      cidr_blocks      = local.ALL_CIDR
+      ipv6_cidr_blocks = []
+      self             = false
+      security_groups =  []
+      prefix_list_ids =  []
+
+    },
+    {
+      description      = "SSH"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = local.ALL_CIDR
+      ipv6_cidr_blocks = []
+      self             = false
+      security_groups  = []
+      prefix_list_ids  = []
+    }
+  ]
+
+  egress {
+    description      = "egress"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    self             = false
+    security_groups =  []
+    prefix_list_ids =  []
+  }
+
+
+  tags = {
+    Name = "rabbitmq-${var.ENV}"
+  }
+}
+
+resource "aws_spot_instance_request" "rabbitmq" {
+  ami                    =data.aws_ami.ami.id
+  instance_type          =var.RABBITMQ_INSTANCE_TYPE
+  vpc_security_group_ids = [aws_security_group.rabbitmq.id]
+  wait_for_fulfillment   = true
+  subnet_id              = data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS[0]
+
+  tags = {
+    Name = "rabbitmq-${var.ENV}"
+  }
+}
+resource "aws_ec2_tag" "rabbitmq" {
+  key         = "Name"
+  resource_id = aws_spot_instance_request.rabbitmq.spot_instance_id
+  value       = "rabbitmq-${var.ENV}"
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = data.terraform_remote_state.VPC.outputs.INTERNAL_HOSTEDZONE_ID
+  name    = "rabbitmq-${var.ENV}"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_spot_instance_request.rabbitmq.private_ip]
+}
+
+resource "null_resource" "rabbitmq_setup" {
+  provisioner "remote-exec" {
+    connection {
+      host     = aws_spot_instance_request.rabbitmq.private_ip
+      user     = local.ssh_user
+      password = local.ssh_pass
+    }
+
+
+    inline = [
+      "sudo yum install python3-pip -y",
+      "sudo pip3 install pip --upgrade",
+      "sudo pip3 install ansible",
+      "ansible-pull -U https://github.com/vasubandaru1/ANSIBLE2.git roboshop-pull.yml -e ENV=${var.ENV} -e COMPONENT=rabbitmq"
+
+    ]
+
+  }
+}
+
+
