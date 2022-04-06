04c0d558d74c9fea1661328d27dd15471de8be83
vasu <bandaruvasu1@gmail.com>
2022-04-06 23:15:34 +0530

    parameter and subnet group

--- dba/mysql-rds.tf
+++ dba/mysql-rds.tf
@@ -9,22 +9,19 @@
 #  parameter_group_name = aws_db_parameter_group.pg.name
 #  skip_final_snapshot  = true
 #}
-#
-#resource "aws_db_parameter_group" "pg" {
-#  name   = "mysql-${var.ENV}-pg"
-#  family = "mysql5.7"
-#
-#}
-#
-#resource "aws_db_subnet_group" "subnet-group" {
-#  name       = "mysql-${var.ENV}-"
-#  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
-#
-#  tags = {
-#    Name = "My DB subnet group"
-#  }
-#}
 
-output "PRIVATE_SUBNETS" {
-  value = data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS
+resource "aws_db_parameter_group" "pg" {
+  name   = "mysql-${var.ENV}-pg"
+  family = "mysql5.7"
+
 }
+
+resource "aws_db_subnet_group" "subnet-group" {
+  name       = "mysql-${var.ENV}-subnetgroup"
+  subnet_ids = data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS
+
+  tags = {
+    Name = "mysql-${var.ENV}-subnetgroup"
+  }
+}
+
