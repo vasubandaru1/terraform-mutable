ad3b950644678ee4e36be3d48bc6bb6d835d4b85
vasu <bandaruvasu1@gmail.com>
2022-04-06 10:53:32 +0530

    redis2

--- app-module/ec2.tf
+++ app-module/ec2.tf
@@ -5,6 +5,9 @@ resource "aws_spot_instance_request" "spot" {
   subnet_id             = element(data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS,count.index)
   wait_for_fulfillment = true
   vpc_security_group_ids = [aws_security_group.sg.id]
+  tags = {
+    Name = local.tags["Name"]
+  }
 }
 
 resource "aws_instance" "od" {
