0e4805e8814790f61ecded466de4b105cc64e4f0
vasu <bandaruvasu1@gmail.com>
2022-04-07 13:05:26 +0530

    parameter and subnet group

--- dba/mongo-ec2.tf
+++ dba/mongo-ec2.tf
@@ -55,7 +55,7 @@ resource "aws_spot_instance_request" "mongodb" {
   instance_type = var.MONGODB_INSTANCE_TYPE
   vpc_security_group_ids = [aws_security_group.mongodb.id]
   wait_for_fulfillment = true
-  subnet_id = data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNET_IDS[0]
+  subnet_id = data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS_IDS[0]
   tags = {
     Name = "mongodb-${var.ENV}"
   }
